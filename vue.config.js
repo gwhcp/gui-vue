24e648d37983ab033b77a3770af4d011ebe48890
Sammie S. Taunton <diemuzi@gmail.com>
2021-11-11 13:33:06 -0600

    Added Title

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = process.env.VUE_APP_API_TITLE;
+
+                return args;
+            });
+    }
+}
